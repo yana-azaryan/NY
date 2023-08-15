88725f7315bc14f891cf03c7ababd86226f7d707
Yana <administrator@yana-azaryan-mac.local>
2023-08-15 16:38:35 +0400

    Updates

---
+++ precache-manifest.da40dd4e89d319d1203bf9cf0057a1e2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c42d612f9c6b18b033135a6380e4a60d",
+    "url": "/NY/index.html"
+  },
+  {
+    "revision": "c016e53e299c99497f49",
+    "url": "/NY/static/css/2.bc1e5c15.chunk.css"
+  },
+  {
+    "revision": "ed95c10067ce55cb1d3f",
+    "url": "/NY/static/css/main.5a46e529.chunk.css"
+  },
+  {
+    "revision": "c016e53e299c99497f49",
+    "url": "/NY/static/js/2.4a58756b.chunk.js"
+  },
+  {
+    "revision": "ed95c10067ce55cb1d3f",
+    "url": "/NY/static/js/main.0b4d4e91.chunk.js"
+  },
+  {
+    "revision": "13e4edc9dcbc5d872e79",
+    "url": "/NY/static/js/runtime~main.b59e96e5.js"
+  },
+  {
+    "revision": "9356cc5b1dea01cc4af52ee36cfc8d85",
+    "url": "/NY/static/media/1.9356cc5b.jpeg"
+  },
+  {
+    "revision": "906da2b86f6b721736ea5229cffc91f0",
+    "url": "/NY/static/media/2.906da2b8.jpeg"
+  },
+  {
+    "revision": "7652337b1c5384c6207d0a49eb94fc76",
+    "url": "/NY/static/media/3.7652337b.jpeg"
+  },
+  {
+    "revision": "38d3c315c92d1dd399bac8e53a92b785",
+    "url": "/NY/static/media/4.38d3c315.jpeg"
+  },
+  {
+    "revision": "538e74e79b4a5f45dedb981913e3883d",
+    "url": "/NY/static/media/5.538e74e7.jpeg"
+  },
+  {
+    "revision": "95efd987d910d375b7eb33d4c61f14bf",
+    "url": "/NY/static/media/6.95efd987.jpeg"
+  },
+  {
+    "revision": "0848d9d0f415ea63d3ecce98e586b461",
+    "url": "/NY/static/media/counter-1-1600x600.0848d9d0.jpg"
+  },
+  {
+    "revision": "ae831ff012cab52b6931e8c91e001f73",
+    "url": "/NY/static/media/invite.ae831ff0.png"
+  }
+]);
